5814fc13399ae63692e57075391669d84c891caf
Eduardo Tavares <edutav.dev@gmail.com>
2024-06-24 00:20:12 -0300

    relacionamentos entre tabelas

---
+++ prisma/migrations/20240624031723_create_relationships/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `users` table. All the data in the column will be lost.
+  - Added the required column `gym_id` to the `check_ins` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `user_id` to the `check_ins` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "check_ins" ADD COLUMN     "gym_id" TEXT NOT NULL,
+ADD COLUMN     "user_id" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "createdAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AddForeignKey
+ALTER TABLE "check_ins" ADD CONSTRAINT "check_ins_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "check_ins" ADD CONSTRAINT "check_ins_gym_id_fkey" FOREIGN KEY ("gym_id") REFERENCES "gyms"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
