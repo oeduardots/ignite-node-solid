7831194ae0efc5ef98e659b3dba2e02cc5d25886
Eduardo Tavares <edutav.dev@gmail.com>
2024-07-27 14:28:30 -0300

    primeiro teste unitario

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,26 @@
+import { compare } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
+
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+
+import { RegisterUseCase } from './register'
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johondoe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+})
